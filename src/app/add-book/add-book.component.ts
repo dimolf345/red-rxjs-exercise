8291cf143f6af94b58599f47530fd99d60519e05
Luca Di Molfetta <63596667+dimolf345@users.noreply.github.com>
2023-09-19 09:34:59 +0200

    completed newbook form html template

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.scss']
+  styleUrls: ['./add-book.component.scss'],
 })
-export class AddBookComponent {
+export class AddBookComponent implements OnInit {
+  newBookForm!: FormGroup;
+  coverImg: string = '';
+  atlText: string = '';
 
+  constructor(private Router: Router) {}
+
+  ngOnInit(): void {
+    this.createForm();
+    this.newBookForm.get('imageUrl')?.valueChanges.subscribe((url) => {
+      this.atlText = this.newBookForm.get('title')?.value
+        ? this.newBookForm.get('title')?.value + ' cover'
+        : '';
+      this.coverImg = url;
+    });
+  }
+
+  createForm() {
+    this.newBookForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      author: new FormControl('', Validators.required),
+      pubblicationYear: new FormControl('', [
+        Validators.pattern(/^[12]?\d{3}$/),
+      ]),
+      description: new FormControl(''),
+      imageUrl: new FormControl('', {
+        updateOn: 'blur',
+      }),
+      pages: new FormControl(1, [Validators.min(1)]),
+    });
+  }
 }
--- src/app/add-book/add-book.module.ts
+++ src/app/add-book/add-book.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AddBookRoutingModule } from './add-book-routing.module';
 import { AddBookComponent } from './add-book.component';
 import { AddbookFormComponent } from '../components/addbook-form/addbook-form.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AddBookComponent,
-    AddbookFormComponent
-  ],
-  imports: [
-    CommonModule,
-    AddBookRoutingModule
-  ]
+  declarations: [AddBookComponent, AddbookFormComponent],
+  imports: [CommonModule, AddBookRoutingModule, ReactiveFormsModule],
 })
-export class AddBookModule { }
+export class AddBookModule {}
